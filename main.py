ebd1a7bc5981644952d01f45199fca4157e84210
IsSantiagoFL <drakesklr@gmail.com>
2023-07-19 19:29:51 +0000

    Primer commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# Esto es un comentario simple de una linea
+
+"""
+Este es un 
+comentario largo de 
+varias lineas
+"""
+
+'''
+Esta es otra 
+forma al estilo 
+python, de hacer 
+un comentario de 
+varias lineas
+'''
+
+# Imprimir mensajes en consola:
+print("Hola mundo")
+
+# Operaciones aritmeticas en python:
+print(10-5)
+print(8/2)
+
